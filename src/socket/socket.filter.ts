976e0b0d0101960c68dbf2cde73acd332490f5cd
jsween5723 <jsween5723@gmail.com>
2023-10-17 03:34:25 +0900

    fix(예외처리): disconnect는 클라이언트에서 하도록

--- src/socket/socket.filter.ts
+++ src/socket/socket.filter.ts
@@ -7,6 +7,5 @@ export class WsExceptionFilter extends BaseWsExceptionFilter {
   catch(exception: WsException, host: ArgumentsHost) {
     const client = host.switchToWs().getClient<Socket>();
     client.emit('error', { code: exception.name, message: exception.message });
-    client.disconnect(true);
   }
 }

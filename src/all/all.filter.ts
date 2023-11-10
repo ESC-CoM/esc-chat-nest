a85f609656ebb1d562922090cb67e26eeeda0ea2
jsween5723 <jsween5723@gmail.com>
2023-11-11 08:39:52 +0900

    fix(cors): fix

--- src/all/all.filter.ts
+++ src/all/all.filter.ts
@@ -10,31 +10,17 @@ import { HttpAdapterHost } from '@nestjs/core';
 import { Socket } from 'socket.io';
 import { Error } from 'mongoose';
 import { WsException } from '@nestjs/websockets';
-import e, { Request, Response } from 'express';
+import e, { json, Request, response, Response } from 'express';
 import { BaseErrorResponse, BaseResponse } from '../common/base-response';
 
 @Catch()
 export class AllExceptionsFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost): void {
     const ctx = host.switchToHttp();
-    const request = ctx.getRequest<Request>();
-    let hostname = request.headers.referer;
-    if (host.getType() == 'http') {
-      console.log(host.getType());
-      console.log(hostname);
-      const response = ctx
-        .getResponse<Response>()
-        .status(
-          exception instanceof HttpException ? exception.getStatus() : 500,
-        );
-      if (hostname) {
-        if (hostname.endsWith('/')) {
-          hostname = hostname.slice(0, -1);
-        }
-        response.setHeader('Access-Control-Allow-Origin', hostname);
-      }
-      response.json(new BaseErrorResponse(exception));
-    }
+    ctx
+      .getResponse<Response>()
+      .status(exception instanceof HttpException ? exception.getStatus() : 500)
+      .json(new BaseErrorResponse(exception));
   }
 
   catchWs(exception: WsException, host: ArgumentsHost) {
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { IoAdapter } from '@nestjs/platform-socket.io';
 import * as process from 'process';
 import { AllExceptionsFilter } from './all/all.filter';
+import * as https from 'https';
 
 export const CONFIG_URL = `${process.env.SCHEME}://${process.env.ESC_CONFIG}`;
 
@@ -12,9 +13,15 @@ export async function bootstrap() {
   const result = await fetch(`${CONFIG_URL}/meeting/default`).then((result) =>
     result.json(),
   );
-  const sourceElement = result.propertySources[0].source['cors-list'];
+  const sourceElement =
+    result.propertySources[0].source['cors-list'].split(', ');
   const app = await NestFactory.create(AppModule, {
-    cors: true,
+    cors: {
+      origin: sourceElement,
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
+      credentials: true,
+    },
   });
   app.useGlobalFilters(new AllExceptionsFilter());
   const ioAdapter = new IoAdapter(app);

6c60a48c627554613196f5bb23921b87ece89abc
juyeong-chungbuk <happyu9986@gmail.com>
2024-08-24 17:32:13 +0900

    feat: 특정 room에 join하도록

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,11 +1,12 @@
+import { Logger } from '@nestjs/common';
 import {
+  ConnectedSocket,
   MessageBody,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
-import { Socket } from 'net';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway(3600, {
   namespace: 'chat-rooms',
@@ -14,18 +15,34 @@ export class ChatGateway {
   @WebSocketServer()
   public io: Server;
 
+  private logger = new Logger(ChatGateway.name);
+
   handleConnection(client: any) {
     const socketId = client.id;
     this.io.emit('connection', { state: 'success', socketId });
   }
 
-  @SubscribeMessage('message')
-  handleMessage(client: Socket, query: string) {
-    //
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected: ${client.id}`);
   }
 
-  @SubscribeMessage('events')
-  handleEvent(@MessageBody() data: string): string {
-    return data;
+  @SubscribeMessage('room-join')
+  handleJoinRoom(
+    @MessageBody() data: { roomId: string },
+    @ConnectedSocket() client: Socket,
+  ) {
+    const { roomId } = data;
+    client.join(roomId);
+    this.io.emit('room-join', { state: 'success' });
+  }
+
+  @SubscribeMessage('room-exit')
+  handleLeaveRoom(
+    @MessageBody() data: { roomId: string },
+    @ConnectedSocket() client: Socket,
+  ) {
+    const { roomId } = data;
+    client.leave(roomId);
+    this.io.emit('room-exit', { state: 'success' });
   }
 }
